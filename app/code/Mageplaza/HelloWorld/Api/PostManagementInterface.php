12cbd16bbfebe3c4998e82d3bd0af687fa53619e
Thai Nguyen <thai.nguyen@aegona.com>
2023-04-23 16:01:37 +0700

    #

---
+++ app/code/Mageplaza/HelloWorld/Api/PostManagementInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Mageplaza\HelloWorld\Api;
+
+
+interface PostManagementInterface
+{
+
+
+    /**
+     * GET for Post api
+     * @param string $param
+     * @return string
+     */
+
+    public function getPost($param);
+}
---
+++ app/code/Mageplaza/HelloWorld/Model/PostManagement.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Mageplaza\HelloWorld\Model;
+
+
+class PostManagement
+{
+    public function getPost($param)
+    {
+        return 'api GET return the $param ' . $param;
+    }
+}
